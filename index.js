b6d5c253de808be0f4dab7d17471a33561010b9b
Pranto Deb <rajprit0m@outlook.com>
2025-03-31 02:38:51 +0600

    [Server Create]

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
