45adbc78e4dd5663504491a15d8bbef18f93b1eb
Pranto Deb <rajprit0m@outlook.com>
2025-03-31 02:46:27 +0600

    [Create a MongoClient]

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-require("dotenv").config();
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Create a MongoClient
+const client = new MongoClient(process.env.DB_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
